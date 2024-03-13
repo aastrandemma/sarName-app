f23ff0af20f852697d744b25727c3601173b4383
Emma Åstrand <aastrand.emma@gmail.com>
2024-03-13 09:17:57 +0100

    Initial commit

---
+++ src/main/java/com/github/aastrandemma/App.java
@@ -0,0 +1,13 @@
+package com.github.aastrandemma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
